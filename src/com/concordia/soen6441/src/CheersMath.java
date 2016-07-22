4b85912295b380eeeb1fdf333418883ebd255db7
m_japa <m_japa@U.ENCS.concordia.ca>
2016-07-22 16:21:20 -0400

    Cos function is modified. Our own decimal format method need to implement.

--- src/com/concordia/soen6441/src/CheersMath.java
+++ src/com/concordia/soen6441/src/CheersMath.java
@@ -1,106 +1,121 @@
 package com.concordia.soen6441.src;
 
 public class CheersMath {
-	
+
 	private int precision;
 	private double pi;
 	private double alpha = 2.304129659127962;
-	
-	public CheersMath(int precision){
-		this.precision = precision; // TODO: cast precision to long only, no double, string or float allowed
+
+	public CheersMath(int precision) {
+		this.precision = precision; // TODO: cast precision to long only, no
+									// double, string or float allowed
 		this.pi = pi();
 	}
-	
-	// Computing square root using Babylonian 
+
+	// Computing square root using Babylonian
 	protected double sqrt(double number) {
-	    double x = number;
-	    double y = 1;
-	    double e = 0.000000000001; // for accuracy level
-	    while (x - y > e) {
-	        x = (x + y) / 2;
-	        y = number / x;
-	    }
-	    return x;
+		double x = number;
+		double y = 1;
+		double e = 0.000000000001; // for accuracy level
+		while (x - y > e) {
+			x = (x + y) / 2;
+			y = number / x;
+		}
+		return x;
 	}
-	
-	protected int factorial(int n){
-	    if(n == 0)
-	        return 1;
-	    else 
-	        return n * factorial(n-1);
+
+	protected int factorial(int n) {
+		if (n == 0)
+			return 1;
+		else
+			return n * factorial(n - 1);
 	}
-	
+
 	protected double degToRad(double deg) {
 		return deg / 180.0 * pi;
 	}
-	
-	protected double round(double nb){
-		double prec = 10^this.precision;
-//		if(precision != null)
-//			prec = 2;
+
+	protected double round(double nb) {
+		double prec = 10 ^ this.precision;
+		// if(precision != null)
+		// prec = 2;
 		nb *= prec;
 		nb = (int) nb;
 		nb /= prec;
 		return nb;
 	}
-	
+
 	// 1 - x^2/2! + x^4/4! - x^6/6! + ...
 	// Computing cos(x) using Taylor Series
 	protected double cos(double x) {
-	    double cosValue = 1.0, power = 1.0;
-	    int n = 2, factorial = 1;
-	    while (n <= this.precision) {
-	        power = power * x * x * -1;
-	        factorial = factorial * n * (n-1);
-	        cosValue = cosValue + (power / factorial);
-	        n = n + 2;
-	    }
-	    return cosValue;
+		int i = 1;
+		double sum = 1.0, term = 1.0;
+		do {
+			term = (term * (x / i));
+			if (i % 2 == 0) {
+				if (i % 4 == 0) {
+					sum = sum + term;
+
+				} else {
+					sum = sum - term;
+				}
+			}
+			i++;
+		} while (i <= 25);
+		return sum;
 	}
-	
+
 	// 4(1 - 1/3 + 1/5 - 1/7 + ...)
 	// Gregory–Leibniz
 	// http://functions.wolfram.com/Constants/Pi/02/
 	protected double pi() {
-	    double piValue = 0, flip = -1, prec = 1000000000;
-	    int n = 1;
-	    while (n <= prec) {
-	    	flip = -1 * flip;
-	        piValue = piValue + (flip / n);
-	        n = n + 2;
-	    }
-	    return 4 * piValue;
+		double piValue = 0, flip = -1, prec = 1000000000;
+		int n = 1;
+		while (n <= prec) {
+			flip = -1 * flip;
+			piValue = piValue + (flip / n);
+			n = n + 2;
+		}
+		return 4 * piValue;
 	}
-	
+
 	// http://mathcentral.uregina.ca/QQ/database/QQ.09.00/roble1.html
-	// using Newton's method, we can solve f(x) = sin(alpha) - alpha + pi/2 when f(x) = 0
-	// Wolfram alpha gives 2.3098814600100572609 https://www.wolframalpha.com/input/?i=alpha+-+sin+alpha+%3D+pi+%2F2
-	protected double alpha(){
+	// using Newton's method, we can solve f(x) = sin(alpha) - alpha + pi/2 when
+	// f(x) = 0
+	// Wolfram alpha gives 2.3098814600100572609
+	// https://www.wolframalpha.com/input/?i=alpha+-+sin+alpha+%3D+pi+%2F2
+	protected double alpha() {
 		double alpha = 1.0;
-		for(int i=0; i<100; i++){ // after 78th iteration, it converges to 2.304129659127962
-			alpha = alpha - ((pi/2 - alpha + sin(alpha))/(-1 + cos(alpha)));
+		for (int i = 0; i < 100; i++) { // after 78th iteration, it converges to
+										// 2.304129659127962
+			alpha = alpha - ((pi / 2 - alpha + sin(alpha)) / (-1 + cos(alpha)));
 		}
 		return alpha; // 2.304129659127962
 	}
-	
-	// Computing sin(x) using Taylor Series
-	protected double sin(double x){		
-        double term = 1.0;
-        double sum  = 0.0;
-        x = x % (2 * pi); //converts value to an angle between negative 2 pi to positive 2 pi
 
-        for (int i = 1; term != 0.0; i++) {
-            term *= (x / i);
-            if (i % 4 == 1) sum += term;
-            if (i % 4 == 3) sum -= term;
-        }
-        return round(sum);
+	// Computing sin(x) using Taylor Series
+	protected double sin(double x) {
+		double term = 1.0;
+		int i = 0;
+		double sum = 0.0;
+		x = x % (2 * pi); // converts value to an angle between negative 2 pi to
+							// positive 2 pi
+
+		for (i = 1; term != 0.0; i++) {
+			term *= (x / i);
+			if (i % 4 == 1)
+				sum += term;
+			if (i % 4 == 3)
+				sum -= term;
+		}
+		return round(sum);
 	}
-	
-	//	L = 2R(1 – cos(α/2))
-	//	α – sin(α) = π/2.
-	public double length(double radius){
+
+	// L = 2R(1 – cos(α/2))
+	// α – sin(α) = π/2.
+	public double length(double radius) {
 		this.precision = 2;
-		return round(2 * radius * (1 - cos(alpha/2))); 
+		double cosValue = cos(alpha / 2);
+		return round(2 * radius * (1 - cosValue));
 	}
 }
--- src/com/concordia/soen6441/ui/CheersUi.java
+++ src/com/concordia/soen6441/ui/CheersUi.java
@@ -2,39 +2,47 @@ package com.concordia.soen6441.ui;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
-import com.concordia.soen6441.src.*;
+import com.concordia.soen6441.src.CheersException;
+import com.concordia.soen6441.src.CheersMath;
 
 public class CheersUi {
 
 	public static void main(String args[]) throws CheersException {
 		try {
-			float radius = 0;
+			double radius = 0;
 			int precision = 0;
 			// Get user input
 			while (true) {
-				InputStreamReader is = new InputStreamReader(System. in );
+				InputStreamReader is = new InputStreamReader(System.in);
 				BufferedReader br = new BufferedReader(is);
 				System.out.println("Please enter the Radius:");
-				try{
-					 // TODO: make parseFloat native and control radius not to be too much small or big 
-					// check from internet what is the smallest-biggest size to add as fact
-					radius = Float.parseFloat(br.readLine());
+				try {
+					// TODO: make parseFloat native and control radius not to be
+					// too much small or big
+					// check from Internet what is the smallest-biggest size to
+					// add as fact
+					// TODO: Come up with our own decimal format function
+					radius = Double.parseDouble(br.readLine());
 					System.out.println("Please enter the Precision:");
 					precision = Integer.parseInt(br.readLine());
-					CheersMath cheersMath = new CheersMath(precision); 
-					System.out.println("The two coasters need to be moved " + cheersMath.length(radius) + " inch far from each other");
-				}
-				catch(NumberFormatException e){
+					CheersMath cheersMath = new CheersMath(precision);
+					DecimalFormat df = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+					df.setMaximumFractionDigits(340); // 340 is the DOUBLE_FRACTION_DIGITS
+					System.out.println("The two coasters need to be moved " + df.format(cheersMath.length(radius))
+							+ " inch far from each other");
+				} catch (NumberFormatException e) {
 					System.out.println("Wrong Number Format");
 				}
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("Cheers UI Exception: " + e);
-		}
-		finally{
-			System.out.println("Program ended");	
+		} finally {
+			System.out.println("Program ended");
 		}
 	}
+
 }
