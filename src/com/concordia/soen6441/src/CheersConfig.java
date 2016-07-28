53edc9204d2fb978b1db003aab1b79a7e261892a
m_japa <m_japa@ROTBLAT.ENCS.concordia.ca>
2016-07-27 23:07:32 -0400

    Added CheersConfig

---
+++ src/com/concordia/soen6441/src/CheersConfig.java
@@ -0,0 +1,19 @@
+package com.concordia.soen6441.src;
+
+public class CheersConfig {
+	public final static double ALPHA = 2.304129659127962;
+	public final static int PRECISION_OUTPUT_MIN = 1;
+	public final static int PRECISION_OUTPUT_MAX = 3;
+	public final static int RADIUS_MIN = 2;
+	public final static int RADIUS_MAX = 5;
+	public final static double STRAIGHT_ANGLE_SIZE = 180.0;
+	
+	public final static double SQRT_PRECISION = 0.000000000001;
+	public final static int PI_PRECISION = 1000000000;
+	
+	public final static int COS_ITERATIONS = 25;
+	public final static int ALPHA_ITERATIONS = 100;
+	
+	public static final int OPERATION_CALCULATE = 1;
+	public static final int OPERATION_EXIT = 2;
+}
