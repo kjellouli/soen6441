2d34696838f4c4675a14c12df6708d80e017553e
KmZ mac <kmz.jello@gmail.com>
2016-07-29 21:04:20 -0400

    FIX config files javadoc

--- src/com/concordia/soen6441/incarnation2/CheersConfig_I2.java
+++ src/com/concordia/soen6441/incarnation2/CheersConfig_I2.java
@@ -1,45 +1,20 @@
 package com.concordia.soen6441.incarnation2;
 
 /**
- * The Class CheersConfig_I2.
- * @author m_japa
- *
+ * The Class CheersConfig_I2
+ * Configuration file containing all constants
  */
 public class CheersConfig_I2 {
-	
-	/** The Constant ALPHA. */
 	public final static double ALPHA = 2.304129659127962;
-	
-	/** The Constant PRECISION_OUTPUT_MIN. */
 	public final static int PRECISION_OUTPUT_MIN = 1;
-	
-	/** The Constant PRECISION_OUTPUT_MAX. */
 	public final static int PRECISION_OUTPUT_MAX = 5;
-	
-	/** The Constant RADIUS_MIN. */
 	public final static int RADIUS_MIN = 1;
-	
-	/** The Constant RADIUS_MAX. */
 	public final static int RADIUS_MAX = 5;
-	
-	/** The Constant STRAIGHT_ANGLE_SIZE. */
 	public final static double STRAIGHT_ANGLE_SIZE = 180.0;
-	
-	/** The Constant SQRT_PRECISION. */
 	public final static double SQRT_PRECISION = 0.000000000001;
-	
-	/** The Constant PI_PRECISION. */
 	public final static int PI_PRECISION = 1000000000;
-	
-	/** The Constant COS_ITERATIONS. */
 	public final static int COS_ITERATIONS = 25;
-	
-	/** The Constant ALPHA_ITERATIONS. */
 	public final static int ALPHA_ITERATIONS = 100;
-	
-	/** The Constant OPERATION_CALCULATE. */
 	public static final int OPERATION_CALCULATE = 1;
-	
-	/** The Constant OPERATION_EXIT. */
 	public static final int OPERATION_EXIT = 2;
 }
